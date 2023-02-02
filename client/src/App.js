7cc840f811c199872a66d1c0212b8c324ffeb6ed
yasinsametkaraca <80101882+yasinsametkaraca@users.noreply.github.com>
2023-02-02 15:56:38 +0300

    project created

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import {Button} from "antd";
+
+function App() {
+   return (
+      <div>
+         <Button type="primary">Primary Button</Button>
+         <h1 className="text-3xl font-bold underline text-red-500">
+            Hello world!
+         </h1>
+      </div>
+   );
+}
+
+export default App;
+//format document = Ctrl+Alt+L
